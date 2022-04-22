7219b8b270eacd65cb4c95ca5d5358c6120e0a65
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2022-04-21 21:58:47 -0300

    Desenhando nave do jogador no canvas

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 const canvas = document.getElementById('mainCanvas');
+const ctx = canvas.getContext('2d');
+
+const spaceshipHeight = 40;
+const spaceshipWidth = 40;
+
+let spaceshipX = (canvas.width - spaceshipWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+function drawSpaceship() {
+  let img = document.createElement('img');
+  img.src = './assets/images/player.png';
+
+  ctx.drawImage(
+    img,
+    spaceshipX,
+    canvas.height - spaceshipHeight,
+    spaceshipHeight,
+    spaceshipWidth
+  );
+}
+
+function keyDownHandler(e) {
+  if (e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == 'Right' || e.key == 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSpaceship();
+
+  if (rightPressed) {
+    spaceshipX += 7;
+  } else if (leftPressed) {
+    spaceshipX -= 7;
+  }
+}
+
+setInterval(draw, 10);
