440a7a137ea643328ec9901964b64d8f44fb08bc
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2022-04-23 13:03:56 -0300

    Fazendo a movimentação do jogador com as setas

--- script.js
+++ script.js
@@ -4,52 +4,140 @@ const ctx = canvas.getContext('2d');
 const spaceshipHeight = 40;
 const spaceshipWidth = 40;
 
-let spaceshipX = (canvas.width - spaceshipWidth) / 2;
+let xSpeed = (canvas.width - spaceshipWidth) / 2;
 
 var rightPressed = false;
 var leftPressed = false;
 
-document.addEventListener('keydown', keyDownHandler, false);
-document.addEventListener('keyup', keyUpHandler, false);
+let img = document.createElement('img');
+img.src = './assets/images/player.png';
 
-function drawSpaceship() {
-  let img = document.createElement('img');
-  img.src = './assets/images/player.png';
+// document.addEventListener('keydown', keyDownHandler, false);
+// document.addEventListener('keyup', keyUpHandler, false);
+
+function drawSpaceship(keys) {
+  if (keys.ArrowRight) {
+    xSpeed += playerXSpeed;
+  } else if (keys.ArrowLeft) {
+    xSpeed -= playerXSpeed;
+  }
 
   ctx.drawImage(
     img,
-    spaceshipX,
+    xSpeed,
     canvas.height - spaceshipHeight,
     spaceshipHeight,
     spaceshipWidth
   );
 }
 
-function keyDownHandler(e) {
-  if (e.key == 'Right' || e.key == 'ArrowRight') {
-    rightPressed = true;
-  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-    leftPressed = true;
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
 
-function keyUpHandler(e) {
-  if (e.key == 'Right' || e.key == 'ArrowRight') {
-    rightPressed = false;
-  } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-    leftPressed = false;
+class Player {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+    this.imgSrc = './assets/images/player.png';
+  }
+
+  get type() {
+    return 'player';
+  }
+
+  static create() {
+    return new Player();
   }
 }
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSpaceship();
+const playerXSpeed = 3;
+
+Player.prototype.update = function (keys) {
+  let xSpeed = 0;
 
   if (rightPressed) {
-    spaceshipX += 7;
+    xSpeed += playerXSpeed;
   } else if (leftPressed) {
-    spaceshipX -= 7;
+    xSpeed -= playerXSpeed;
   }
+
+  let img = document.createElement('img');
+  img.src = this.imgSrc;
+
+  ctx.drawImage(
+    img,
+    xSpeed,
+    canvas.height - spaceshipHeight,
+    spaceshipHeight,
+    spaceshipWidth
+  );
+
+  return new Player();
+};
+
+function trackKeys(keys) {
+  let pressed = Object.create(null);
+
+  function track(event) {
+    if (keys.includes(event.key)) {
+      pressed[event.key] = event.type == 'keydown';
+      event.preventDefault();
+    }
+  }
+
+  window.addEventListener('keydown', track);
+  window.addEventListener('keyup', track);
+
+  return pressed;
 }
 
-setInterval(draw, 10);
+function draw(keys) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawSpaceship(keys);
+
+  // new Player().update();
+}
+
+// TODO: Verificar se existe necessidade para a lógica contida nessa função
+function runAnimation(frameFunc) {
+  let lastTime = null;
+
+  function frame(time) {
+    if (lastTime != null) {
+      let timeStep = 1000;
+
+      if (frameFunc(timeStep) == false) {
+        return;
+      }
+    }
+
+    lastTime = time;
+
+    requestAnimationFrame(frame);
+  }
+
+  requestAnimationFrame(frame);
+}
+
+function runLevel() {
+  return new Promise((resolve) => {
+    const animate = (time) => {
+      draw(arrowKeys);
+    };
+
+    let arrowKeys = trackKeys(['ArrowLeft', 'ArrowRight']);
+
+    runAnimation(animate);
+  });
+}
+
+async function runGame() {
+  await runLevel();
+}
+
+runGame();
